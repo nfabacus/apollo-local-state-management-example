469c75f40fa0b86942a25dc04895ccd38bc16e4f
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-25 09:51:08 +0000

    adds sending value from local state

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from "./logo.svg";
 import { Query } from 'react-apollo';
 import { GET_TEXT_COLOR } from "./Client";
 
-const Header = () => (
-  <Query query={GET_TEXT_COLOR}>
-    {({data}) => (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title" style={{ color: data.textColor.value }}>Apollo Link State Demo</h1>
-      </header>
-    )}
-  </Query>
-);
+class Header extends Component {
+  sendColor =(color) => {
+    alert(`Sending color ${color}`);
+  }
+
+  render() {
+    return (
+      <Query query={GET_TEXT_COLOR}>
+        {({data}) => (
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title" style={{ color: data.textColor.value }}>Apollo Link State Demo</h1>
+            <button onClick={() => this.sendColor(data.textColor.value)}>Send Color</button>
+            <br />
+          </header>
+        )}
+      </Query>
+    );
+  }
+};
 
 export default Header;
